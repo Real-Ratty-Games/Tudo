2530f0afca22a45265fd1d92d0f9e5590f846070
NorbertGerberg <161964914+NorbertGerberg@users.noreply.github.com>
2026-05-11 20:35:14 +0200

    Delete Version.hpp

--- Project/Tudo/Core/Include/Version.hpp
+++
@@ -1,9 +0,0 @@
-/*======================================================
-	Copyright (c) 2026 Real Ratty Games.
-	Created by Norbert Gerberg.
-======================================================*/
-#ifndef VERSION_HPP_
-#define VERSION_HPP_
-
-#define TUDO_VERSION 100
-#endif
